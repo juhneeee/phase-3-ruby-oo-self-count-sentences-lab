fe497d69a8798e29139a4f8c16e14c504baa073c
Johnny Chen <johnnyc1314@gmail.com>
2022-05-17 10:44:39 -0400

    commit tree 8ca4fff0115f84e9b52737909e3ab538ef003034

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    (self.split (/[.?!]/)).filter {|string| string != ""}.size
   end
 end
+
+
+#p "This is a string! It has three sentences. Right?".count_sentences
+#p 'This, well, is a sentence. This is too!! And so is this, I think? Woo...'.count_sentences
